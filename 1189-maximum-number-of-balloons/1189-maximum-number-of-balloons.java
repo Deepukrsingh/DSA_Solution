67525afa6480a93f15263ddca2b84a7814ffffed
Deepu Kumar Singh <153116877+Deepukrsingh@users.noreply.github.com>
2026-09-14 10:20:39 +0530

    Time: 17 ms (7.46%), Space: 44.6 MB (7.08%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,36 +1,25 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        String s = "balloon";
-
-        char arr[] = text.toCharArray();
-
-        for (int i = 0; i < arr.length; i++) {
-            if (map.containsKey(arr[i])) {
-                map.put(arr[i], map.get(arr[i]) + 1);
-            } else {
-                map.put(arr[i], 1);
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<text.length();i++){
+            if(map.containsKey(text.charAt(i))){
+                map.put(text.charAt(i),map.get(text.charAt(i))+1);
+            }
+            else{
+                map.put(text.charAt(i),1);
             }
         }
-
-        int count = 0;
-
-        int i = 0;
-
-        while (map.containsKey(s.charAt(i)) &&
-               map.get(s.charAt(i)) > 0) {
-
-            map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
-            i++;
-
-            if (i == s.length()) {
-                count++;
-                i = 0;
-            }
+        String s="balloon";
+        int count=0;
+        int i=0;
+        while(map.containsKey(s.charAt(i)) && map.get(s.charAt(i))>0){
+                map.put(s.charAt(i),map.get(s.charAt(i))-1);
+                    i++;
+                    if(i==s.length()){
+                        count++;
+                        i=0;
+                    }
         }
-
         return count;
     }
 }
